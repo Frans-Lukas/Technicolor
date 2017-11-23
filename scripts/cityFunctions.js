23a385bd0f6fc1f4feb58e03fd065196a057b1b5
Frans-Lukas <franslukas@gmail.com>
2017-11-23 19:15:08 +0100

    road Building is functioning.

--- scripts/cityFunctions.js
+++ scripts/cityFunctions.js
@@ -12,5 +12,22 @@ module.exports = {
         let sources = creep.room.find(FIND_SOURCES_ACTIVE);
         let ctrler = creep.room.controller;
 
+        let spawns = creep.room.find(FIND_STRUCTURES, {
+            filter: function(object){
+                return object.structureType == STRUCTURE_SPAWN;
+            }
+        });
+        let spawn = spawns[0];
+        for (let i = 0; i < sources.length; i++) {
+            let source = sources[i];
+            var ret = PathFinder.search(spawn.pos, source.pos);
+            for (let j = 0; j < ret.path.length; j++) {
+                let pos = ret.path[j];
+                creep.room.createConstructionSite(pos, STRUCTURE_ROAD);
+                //console.log(pos.x + ', ' + pos.y);
+            }
+
+        }
+
     }
 };
