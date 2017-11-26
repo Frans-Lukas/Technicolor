7c5fcd82005fc36081bdeaed50e3dc55ec2f4da0
Frans-Lukas <franslukas@gmail.com>
2017-11-26 15:30:50 +0100

    Wrote transporter class. Now to see if it is working.

--- default/role.transporter.js
+++ default/role.transporter.js
@@ -1,3 +1,5 @@
+var roleFunctions = require('roleFunctions');
+
 /*Transporting role
 * That takes resources from harvesters and gives them to extensions
 * and containers.
@@ -5,6 +7,37 @@
 
 module.exports = {
     run:function(creep){
-        
+        if(creep.carry.energy < creep.carryCapacity){
+            let harvesters = creep.room.find(FIND_MY_CREEPS, {
+                filter: (s) => s.memory.role === 'harvester';
+            });
+            if(harvesters.length == 0){
+                roleFunctions.mine(creep);
+            } else{
+                harvesters.sort(function(a,b){
+                    return (a.carry.energy < b.carry.energy ? 1 : -1)
+                });
+                if(creep.withdraw(harvesters[0], RESOURCE_ENERGY) ==
+                                                ERR_NOT_IN_RANGE){
+                    creep.moveTo(harvesters[0],
+                        {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            }
+        } else{
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_CONTAINER) &&
+                        structure.energy < structure.energyCapacity;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+
     }
 }
